b1899a5434a51ffc0465a0a0f0e10172ca5d0379
N1ewb <nathaniellucero20@gmail.com>
2023-12-03 21:39:15 +0800

    6th

--- src/components/WeatherList.jsx
+++ src/components/WeatherList.jsx
@@ -5,7 +5,6 @@ const WeatherList = ({hourly, location}) => {
   return (
     <>
         <div className='card-container'>
-            <p></p>
             {hourly.time.map((time, index) => (
                 <Weathercard location={location} temperature={hourly.temperature_2m[index]} time={time}/>
             ))}
